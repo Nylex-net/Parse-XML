6eacff6ab0d1263e61be0bb16782d58700dc6c2b
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-07-10 09:19:37 -0700

    Included interfaces as network devices.

--- ParseToHudu.py
+++ ParseToHudu.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
 COMPANY_NAME = "City of Fortuna"
-FILE_NAME = "config-cityhall.fortuna.local-20230707130041.xml"
+FILE_NAME = "config-cityhall.fortuna.local-20230710084951.xml"
 
 # Get parsed XML data from file.
 f = open(FILE_NAME, 'r')
@@ -9,7 +9,6 @@ pfsense = ET.fromstring(''.join(f.readlines()))
 f.close()
 gateways = pfsense.find('gateways')
 
-f = open('network-devices.csv', 'w')
 content = 'company_name,name,IP Address,Configuration,Subscription/Support Renewal,Notes,Role\n'
 
 for gateway in gateways.findall('gateway_item'):
@@ -20,5 +19,13 @@ for gateway in gateways.findall('gateway_item'):
     content += f"{COMPANY_NAME},{name},{ip},{config},,{desc},Wireless\n"
     # print(f"IP: {ip}, Name: {name}, description: {desc}")
 
+interfaces = pfsense.find('interfaces')
+wan = interfaces.find('wan')
+lan = interfaces.find('lan')
+
+content += f"{COMPANY_NAME},{wan.find('if').text},{wan.find('ipaddr').text},WAN,,{wan.find('subnet').text},Firewall\n"
+content += f"{COMPANY_NAME},{lan.find('if').text},{lan.find('ipaddr').text},LAN,,{lan.find('subnet').text},Firewall"
+
+f = open('network-devices.csv', 'w')
 f.writelines(content)
 f.close()
